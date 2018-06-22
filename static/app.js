297292b7c861f08836cd966537c145dcb0ed0f1b
gllera <gabriellleragarcia@gmail.com>
2018-06-23 01:34:30 +0200

    localchanges

--- static/app.js
+++ static/app.js
@@ -912,74 +912,73 @@ if (typeof module === 'object' && module.exports) {
 /*jshint browser: true, strict: true, undef: true */
 /*global define: false */
 
-( function( window ) {
+(function (window) {
 
-    'use strict';
-    
-    // class helper functions from bonzo https://github.com/ded/bonzo
-    
-    function classReg( className ) {
-      return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
-    }
-    
-    // classList support for class management
-    // altho to be fair, the api sucks because it won't accept multiple classes at once
-    var hasClass, addClass, removeClass;
-    
-    if ( 'classList' in document.documentElement ) {
-      hasClass = function( elem, c ) {
-        return elem.classList.contains( c );
-      };
-      addClass = function( elem, c ) {
-        elem.classList.add( c );
-      };
-      removeClass = function( elem, c ) {
-        elem.classList.remove( c );
-      };
-    }
-    else {
-      hasClass = function( elem, c ) {
-        return classReg( c ).test( elem.className );
-      };
-      addClass = function( elem, c ) {
-        if ( !hasClass( elem, c ) ) {
-          elem.className = elem.className + ' ' + c;
-        }
-      };
-      removeClass = function( elem, c ) {
-        elem.className = elem.className.replace( classReg( c ), ' ' );
-      };
-    }
-    
-    function toggleClass( elem, c ) {
-      var fn = hasClass( elem, c ) ? removeClass : addClass;
-      fn( elem, c );
-    }
-    
-    var classie = {
-      // full names
-      hasClass: hasClass,
-      addClass: addClass,
-      removeClass: removeClass,
-      toggleClass: toggleClass,
-      // short names
-      has: hasClass,
-      add: addClass,
-      remove: removeClass,
-      toggle: toggleClass
+  'use strict';
+
+  // class helper functions from bonzo https://github.com/ded/bonzo
+
+  function classReg(className) {
+    return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+  }
+
+  // classList support for class management
+  // altho to be fair, the api sucks because it won't accept multiple classes at once
+  var hasClass, addClass, removeClass;
+
+  if ('classList' in document.documentElement) {
+    hasClass = function (elem, c) {
+      return elem.classList.contains(c);
     };
-    
-    // transport
-    if ( typeof define === 'function' && define.amd ) {
-      // AMD
-      define( classie );
-    } else {
-      // browser global
-      window.classie = classie;
-    }
-    
-    })( window );
-    
+    addClass = function (elem, c) {
+      elem.classList.add(c);
+    };
+    removeClass = function (elem, c) {
+      elem.classList.remove(c);
+    };
+  } else {
+    hasClass = function (elem, c) {
+      return classReg(c).test(elem.className);
+    };
+    addClass = function (elem, c) {
+      if (!hasClass(elem, c)) {
+        elem.className = elem.className + ' ' + c;
+      }
+    };
+    removeClass = function (elem, c) {
+      elem.className = elem.className.replace(classReg(c), ' ');
+    };
+  }
+
+  function toggleClass(elem, c) {
+    var fn = hasClass(elem, c) ? removeClass : addClass;
+    fn(elem, c);
+  }
+
+  var classie = {
+    // full names
+    hasClass: hasClass,
+    addClass: addClass,
+    removeClass: removeClass,
+    toggleClass: toggleClass,
+    // short names
+    has: hasClass,
+    add: addClass,
+    remove: removeClass,
+    toggle: toggleClass
+  };
+
+  // transport
+  if (typeof define === 'function' && define.amd) {
+    // AMD
+    define(classie);
+  } else {
+    // browser global
+    window.classie = classie;
+  }
+
+})(window);
+
 (function () {
     const debounce = function (func, delay) {
         var inDebounce = undefined
@@ -1044,7 +1043,7 @@ if (typeof module === 'object' && module.exports) {
         })();
     }
 
-    [].slice.call(document.querySelectorAll('input.input__field')).forEach(function (inputEl) {
+    [].slice.call(document.querySelectorAll('.input input')).forEach(function (inputEl) {
         // in case the input is already filled..
         if (inputEl.value.trim() !== '') {
             classie.add(inputEl.parentNode, 'input--filled');
