73958110934cb83ea840e09eae7bfa43e4683edf
gllera <gabriellleragarcia@gmail.com>
2018-08-21 00:50:27 +0200

    Testing

--- .circleci/builder.sh
+++ .circleci/builder.sh
@@ -1,6 +1,7 @@
 HUGO_VERSION=0.47.1
 HUGO_BINARY=hugo_extended_${HUGO_VERSION}_Linux-64bit.deb
 
+sudo apt-get install libstdc++6
 mkdir     -p ${CIRCLE_WORKING_DIRECTORY}/.binaries
 wget     -qO ${CIRCLE_WORKING_DIRECTORY}/.binaries/hugo.deb https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_BINARY}
 sudo dpkg -i ${CIRCLE_WORKING_DIRECTORY}/.binaries/hugo.deb
