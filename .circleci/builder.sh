3baa7eaf64eaf4d328a85dd0a41eea348b9abc15
gllera <gabriellleragarcia@gmail.com>
2018-08-21 01:26:18 +0200

    Testing

--- .circleci/builder.sh
+++ .circleci/builder.sh
@@ -1,7 +1,11 @@
 HUGO_VERSION=0.47.1
 HUGO_BINARY=hugo_extended_${HUGO_VERSION}_Linux-64bit.deb
 
+git submodule sync 
+git submodule update --init
+
 wget     -qO /tmp/hugo.deb https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_BINARY}
 sudo dpkg -i /tmp/hugo.deb
 hugo
+
 npm start
