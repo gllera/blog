e95527048d7ccb05419b6c7fea2fc2c0badc3578
gllera <gabriellleragarcia@gmail.com>
2018-08-21 00:34:58 +0200

    more circle-ci testing

--- .circleci/builder.sh
+++ .circleci/builder.sh
@@ -1,1 +1,6 @@
-echo "hola.."
+HUGO_VERSION=0.47.1
+HUGO_BINARY=hugo_extended_${HUGO_VERSION}_Linux-64bit.deb
+
+wget -O .binaries/hugo.deb https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_BINARY}
+dpkg -i .binaries/hugo.deb
+rm   -f .binaries/hugo.deb
