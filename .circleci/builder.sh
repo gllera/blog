b268f8794844f2091593212d143f59026c571c02
gllera <gabriellleragarcia@gmail.com>
2018-08-21 01:34:20 +0200

    Testing

--- .circleci/builder.sh
+++ .circleci/builder.sh
@@ -1,6 +1,8 @@
 HUGO_VERSION=0.47.1
 HUGO_BINARY=hugo_extended_${HUGO_VERSION}_Linux-64bit.deb
 
+git config --global user.email "${GIT_EMAIL}"
+git config --global user.name "${GIT_USERNAME}"
 git submodule sync 
 git submodule update --init
 
@@ -9,3 +11,7 @@ sudo dpkg -i /tmp/hugo.deb
 hugo
 
 npm start
+
+cd dist
+git commit -am "Build from commit ${CIRCLE_SHA1}"
+git push
