0f800cbc0201610dea5c401450c7d5551d74d173
gllera <gabriellleragarcia@gmail.com>
2018-08-21 01:37:39 +0200

    Testing

--- .circleci/builder.sh
+++ .circleci/builder.sh
@@ -5,6 +5,7 @@ git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_USERNAME}"
 git submodule sync 
 git submodule update --init
+(cd dist && git checkout master)
 
 wget     -qO /tmp/hugo.deb https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_BINARY}
 sudo dpkg -i /tmp/hugo.deb
