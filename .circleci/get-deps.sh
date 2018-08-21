669e999a34dcddb551a38882acfa8190702ed266
gllera <gabriellleragarcia@gmail.com>
2018-08-21 21:20:53 +0200

    Testing

--- .circleci/get-deps.sh
+++ .circleci/get-deps.sh
@@ -1,6 +1,8 @@
 HUGO_VERSION=0.47.1
 NODE_VERSION=8.11.4
 
+mkdir /tmp/hugo
+
 wget -qO /tmp/hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
 tar  -xf /tmp/hugo.tar.gz -C /tmp/hugo
 
