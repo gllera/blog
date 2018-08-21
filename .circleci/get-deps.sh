5b9dea5c7752692abb734061d85d708058540c70
gllera <gabriellleragarcia@gmail.com>
2018-08-21 21:21:57 +0200

    Testing

--- .circleci/get-deps.sh
+++ .circleci/get-deps.sh
@@ -6,8 +6,8 @@ mkdir /tmp/hugo
 wget -qO /tmp/hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
 tar  -xf /tmp/hugo.tar.gz -C /tmp/hugo
 
-wget -qO /tmp/node.tar.xz https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-x64.tar.xz
-tar  -xf /tmp/node.tar.xz -C /tmp
+wget -qO /tmp/node.tar.gz https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-x64.tar.gz
+tar  -xf /tmp/node.tar.gz -C /tmp
 
 export NODEJS_HOME=/tmp/node-v${NODE_VERSION}-linux-x64/bin
 export PATH=$NODEJS_HOME:/tmp/hugo:$PATH
