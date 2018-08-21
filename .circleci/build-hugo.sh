7a20c516bd77ce524c864bb6f29e1de56c7f4cf8
gllera <gabriellleragarcia@gmail.com>
2018-08-21 19:49:31 +0200

    Removed unnecessary folders and also avoided non deterministic builds with hugo

--- .circleci/build-hugo.sh
+++ .circleci/build-hugo.sh
@@ -4,3 +4,4 @@ HUGO_BINARY=hugo_extended_${HUGO_VERSION}_Linux-64bit.deb
 wget     -qO /tmp/hugo.deb https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/${HUGO_BINARY}
 sudo dpkg -i /tmp/hugo.deb
 hugo
+(cd public && rm -rf categories post tags)
