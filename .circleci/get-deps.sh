2cece79ed8aea5235f263b31a1265d2ece187fd9
gllera <gabriellleragarcia@gmail.com>
2018-08-21 21:09:53 +0200

    Testing

--- .circleci/get-deps.sh
+++ .circleci/get-deps.sh
@@ -3,11 +3,11 @@ alias curl='curl --silent --show-error --fail'
 HUGO_VERSION=0.47.1
 NODE_VERSION=8.11.4
 
-curl      -o /tmp/hugo.deb https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.deb
-sudo dpkg -i /tmp/hugo.deb
+curl      -o /tmp/hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+tar      -xf /tmp/hugo.tar.gz -C /tmp/hugo
 
 curl      -o /tmp/node.tar.xz https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-x64.tar.xz
 tar      -xf /tmp/node.tar.xz -C /tmp
 
 export NODEJS_HOME=/tmp/node-v${NODE_VERSION}-linux-x64/bin
-export PATH=$NODEJS_HOME:$PATH
+export PATH=$NODEJS_HOME:/tmp/hugo:$PATH
