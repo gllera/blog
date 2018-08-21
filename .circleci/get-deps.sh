da26336232d941e69a72dc6b2e4890eb30e83750
gllera <gabriellleragarcia@gmail.com>
2018-08-21 21:17:01 +0200

    Testing

--- .circleci/get-deps.sh
+++ .circleci/get-deps.sh
@@ -4,6 +4,7 @@ HUGO_VERSION=0.47.1
 NODE_VERSION=8.11.4
 
 curl      -o /tmp/hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+cat /tmp/hugo.tar.gz
 tar      -xf /tmp/hugo.tar.gz -C /tmp/hugo
 
 curl      -o /tmp/node.tar.xz https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-x64.tar.xz
