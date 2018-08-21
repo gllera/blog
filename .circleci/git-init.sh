f5bc46548f7c159699cfc6939be0fccfd303845e
gllera <gabriellleragarcia@gmail.com>
2018-08-21 23:42:36 +0200

    Removed dist submodule

--- .circleci/git-init.sh
+++ .circleci/git-init.sh
@@ -1,5 +1,3 @@
 git config --global user.email "${GIT_EMAIL}" 
 git config --global user.name "${GIT_USERNAME}"
-git submodule sync 
-git submodule update --init
-(cd dist && git checkout master)
+git clone --depth 1 --single-branch https://github.com/gllera/gllera.github.io dist
