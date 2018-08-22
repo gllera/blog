e378a2303f1a72087d6cb6f450b3a51b451e4f33
gllera <gabriellleragarcia@gmail.com>
2018-08-22 18:32:54 +0200

    CI fix

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,6 +1,7 @@
 cd dist 
 if [[ `git status --porcelain` ]]; then
-    git commit -am "Build from commit ${CIRCLE_SHA1}"
+    git add .
+    git commit -m "Build from commit ${CIRCLE_SHA1}"
     git push    
 else
     echo "No changes found"
