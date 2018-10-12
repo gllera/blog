cf9206af5130348a40b531cd357f7b9fa365a29d
gllera <gabriellleragarcia@gmail.com>
2018-10-12 19:10:02 +0200

    ci update

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,7 +1,7 @@
 cd dist 
 if [[ `git status --porcelain` ]]; then
     git add .
-    git commit -m "Build from commit ${CIRCLE_SHA1}"
+    git commit -m "Build from commit ${CI_COMMIT_SHA}"
     git push    
 else
     echo "No changes found"
