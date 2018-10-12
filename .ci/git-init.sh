a0856320c9c5b9a37d2c17923caea20a670fc7e5
gllera <gabriellleragarcia@gmail.com>
2018-10-12 18:58:03 +0200

    testing ci

--- .ci/git-init.sh
+++ .ci/git-init.sh
@@ -1,3 +1,3 @@
 git config --global user.email "${GIT_EMAIL}" 
 git config --global user.name "${GIT_USERNAME}"
-git clone --depth 1 --single-branch https://github.com/gllera/gllera.github.io dist
+git clone --depth 1 --single-branch https://gllera@github.com/gllera/gllera.github.io dist
